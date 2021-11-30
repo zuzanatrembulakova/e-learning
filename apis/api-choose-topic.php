5cbbdd45632cfbdc25e54e9624317664a5c378c8
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-11-30 10:48:40 +0100

    active topics select

--- apis/api-choose-topic.php
+++ apis/api-choose-topic.php
@@ -12,11 +12,12 @@ try {
 
     $timestamp = date('Y-m-d H:i:s');
 
-    $q = $db->prepare('INSERT INTO topic_student VALUES (:topic_student_id, :topic_id, :student_id, :topic_start_date)');
+    $q = $db->prepare('INSERT INTO topic_student VALUES (:topic_student_id, :topic_id, :student_id, :topic_start_date, :topic_end_date)');
     $q->bindValue(":topic_student_id", null);
     $q->bindValue(":topic_id", $_POST['tag']);
     $q->bindValue(":student_id", $_POST['student_id']);
     $q->bindValue(":topic_start_date", $timestamp);
+    $q->bindValue(":topic_end_date", null);
     $q->execute();
 
     _response(200, 'Topic selected', __LINE__);
