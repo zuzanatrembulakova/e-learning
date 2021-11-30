6d953bef88160d77fb632905010cc925fd4b4e33
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-11-30 14:07:08 +0100

    Finished activities and grades added

--- overview.php
+++ overview.php
@@ -6,6 +6,18 @@ require_once(__DIR__.'/globals.php');
 
 $db = _db();
 
+$mng = _dbMongoManager();
+
+$filter = [];
+$options = ['_id' => 0];
+
+$query = new MongoDB\Driver\Query($filter, $options);
+$cursor = $mng->executeQuery('forum.discussion', $query);
+
+$data = json_encode(iterator_to_array($cursor));
+
+var_dump($data);
+
 try {
     $q = $db->prepare('SELECT topic_student_activity.activity_id, topics.topic_name, 
     activities.activity_name, topics.topic_id, topic_student_activity.topic_student_id FROM topic_student_activity 
@@ -16,6 +28,36 @@ try {
     $q->bindValue(':student_id', $_SESSION['student']['student_id']);
     $q->execute();
     $rows = $q->fetchAll();
+
+
+} catch(Exception $ex){
+    _response(500, 'System under maintainance', __LINE__);
+    exit(); 
+}
+
+try {
+    $q = $db->prepare('SELECT activities.* FROM activities
+    INNER JOIN topic_student_activity ON topic_student_activity.activity_id = activities.activity_id
+    INNER JOIN topic_student ON topic_student.topic_student_id = topic_student_activity.topic_student_id
+    WHERE topic_student.student_id = :student_id AND topic_student_activity.activity_end_date IS NOT NULL');
+    $q->bindValue(':student_id', $_SESSION['student']['student_id']);
+    $q->execute();
+    $finishedActivities = $q->fetchAll();
+
+
+} catch(Exception $ex){
+    _response(500, 'System under maintainance', __LINE__);
+    exit(); 
+}
+
+try {
+    $q = $db->prepare('SELECT grades.grade_name FROM grades
+    INNER JOIN topic_student_activity ON topic_student_activity.grade_id = grades.grade_id
+    INNER JOIN topic_student ON topic_student_activity.topic_student_id = topic_student.topic_student_id
+    WHERE topic_student.student_id = :student_id');
+    $q->bindValue(':student_id', $_SESSION['student']['student_id']);
+    $q->execute();
+    $activtyGrades = $q->fetchAll();
 
 
 } catch(Exception $ex){
@@ -47,12 +89,6 @@ try{
     _response(500, 'System under maintainance', __LINE__);
     exit();
 }
-
-// SELECT topic_student_activity.activity_id, topics.topic_name, activities.activity_name FROM topic_student_activity 
-// INNER JOIN topic_student ON topic_student_activity.topic_student_id = topic_student.topic_student_id 
-// INNER JOIN topics ON topic_student.topic_id = topics.topic_id 
-// INNER JOIN activities ON topic_student_activity.activity_id = activities.activity_id 
-// WHERE activity_end_date IS NULL AND topic_student.student_id = 8;
 
 ?>
 
@@ -108,6 +144,19 @@ try{
 
         <div>
             <h2>Finished activities</h2>
+            <div id="started_activities">
+
+                <?php foreach($finishedActivities as $finishedActivity){ ?>
+                    <form onsubmit="return false" id="form_started_activity">
+                        <a href="#"><?= $finishedActivity['activity_name'] ?></a>
+                        <p><?php if($finishedActivity['activity_is_graded'] == 1){
+                            foreach($activtyGrades as $activtyGrade){
+                                echo $activtyGrade['grade_name'];
+                         }} else{ ?>Not graded <?php } ?></p>
+                    </form>
+                <?php } ?>
+
+            </div>
         </div>
 
     </div>
