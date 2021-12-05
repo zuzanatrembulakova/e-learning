f93ccf3974e5ed95adf7fd9f1e9443443d38233e
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-12-05 11:45:36 +0100

    Teacher grades and student finished activities fixed

--- student-finished-activities.php
+++ student-finished-activities.php
@@ -22,11 +22,12 @@ try {
     }
 
 try {
-        $q2 = $db->prepare('SELECT students.student_name, students.student_surname, activities.activity_name, activities.activity_is_graded, activities.activity_id, topic_student_activity.activity_end_date FROM topic_student_activity
+        $q2 = $db->prepare('SELECT students.student_name, students.student_surname, activities.activity_name, 
+        activities.activity_is_graded, activities.activity_id, topic_student_activity.activity_end_date FROM topic_student_activity
         INNER JOIN activities ON topic_student_activity.activity_id = activities.activity_id
-        INNER JOIN topic_student ON activities.topic_id = topic_student.topic_id 
+        INNER JOIN topic_student ON activities.topic_id = topic_student.topic_id AND topic_student_activity.topic_student_id = topic_student.topic_student_id
         INNER JOIN students ON topic_student.student_id = students.student_id 
-        WHERE topic_student_activity.activity_end_date IS NOT NULL AND topic_student.topic_id = :topic_id AND students.student_id = :student_id');
+        WHERE topic_student_activity.activity_end_date IS NOT NULL AND topic_student.topic_id = :topic_id AND topic_student.student_id = :student_id');
         $q2->bindValue(':topic_id', $topicid);
         $q2->bindValue(':student_id', $studentid);
         $q2->execute();
@@ -38,18 +39,14 @@ try {
     }
 
 try {
-    $q3 = $db->prepare('SELECT grades.grade_name, grades.grade_id FROM topic_student_activity
-    INNER JOIN activities ON topic_student_activity.activity_id = activities.activity_id
-    INNER JOIN topic_student ON activities.topic_id = topic_student.topic_id
-    INNER JOIN grades ON topic_student_activity.grade_id = grades.grade_id
-    INNER JOIN students ON topic_student.student_id = students.student_id
-    INNER JOIN topics ON topic_student.topic_id = topics.topic_id
-    WHERE topic_student.topic_id = :topic_id AND students.student_id = :student_id');
-    $q3->bindValue(':topic_id', $topicid);
-    $q3->bindValue(':student_id', $studentid);
-    $q3->execute();
-    $grade = $q3->fetch();
-
+    $q = $db->prepare('SELECT grades.grade_id, grades.grade_name FROM grades
+    INNER JOIN topic_student_activity ON topic_student_activity.grade_id = grades.grade_id
+    INNER JOIN topic_student ON topic_student_activity.topic_student_id = topic_student.topic_student_id 
+    WHERE topic_student.topic_id = :topic_id AND topic_student.student_id = :student_id');
+    $q->bindValue(':topic_id', $topicid);
+    $q->bindValue(':student_id', $studentid);
+    $q->execute();
+    $grade = $q->fetch();
 
 } catch(Exception $ex){
     _response(500, 'System under maintainance', __LINE__);
@@ -80,13 +77,11 @@ try {
         <div>
             <a href="grade-activity.php?activityID=<?= $activity['activity_id'] ?>&studentID=<?= $studentid ?>"><?= $activity['activity_name'] ?></a>
             <?php 
-            if ($grade['grade_id']) {
-            ?>
+            
+            if (isset($grade['grade_id'])) { ?>
                 <p><?= $grade['grade_name'] ?></p>
-            <?php 
-            } else{
-            ?>
-            <p>Graded</p>
+            <?php } else{?>
+                <p>Graded</p>
             <?php 
             } 
             ?>
