fec13f87328ff80e48893d586d32cff783983bca
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2021-12-07 22:17:40 +0100

    Student list added to teacher-topics

--- teacher-topics.php
+++ teacher-topics.php
@@ -30,6 +30,18 @@ try {
     exit(); 
 }
 
+try{
+    $q = $db->prepare('SELECT * FROM topics');
+    $q->execute();
+    $allTopics = $q->fetchAll();
+
+} catch(Exception $ex){
+    _response(500, 'System under maintainance', __LINE__);
+    exit();
+}
+
+
+
 ?> 
 
 <!DOCTYPE html>
@@ -43,19 +55,73 @@ try {
 </head>
 <body>
 
-    
-    <h1>You are logged in as <?= $row['teacher_name'] ?> <?= $row['teacher_surname'] ?></h1>
-    <div id="topics_wrapper">
+    <nav>
+        <h2>Discussion</h2>
         <div>
-        <?php
-        while($topic = $q2->fetch()){
-        ?>
-            <a href="topic-student-list.php?id=<?= $topic['topic_id'] ?>"><?= $topic['topic_name'] ?></a>
-        <?php
-        }
-        ?>
+            <?php foreach($allTopics as $topic){ ?>
+                <a href="discussion.php?id=<?= $topic['topic_id'] ?>"><?= $topic['topic_name'] ?></a>
+            <?php } ?>
         </div>
+    </nav>
+
+    <div id="user">
+        <h1>You are logged in as <?= $row['teacher_name'] ?> <?= $row['teacher_surname'] ?></h1>
+        <a href="bridge-signout.php">Sign out</a>
     </div>
+
+    <table id="topics_wrapper">
+        <tr>
+            <th>Topic</th>
+            <th>Students finished with topic</th>
+        </tr>
+        <?php while($topic = $q2->fetch()){ ?>
+            <tr class='one_topic'>
+                <td class="topic_name"><?= $topic['topic_name'] ?></td>
+                <td id="student_wrapper">
+                    <?php
+                        try {
+
+                            $q = $db->prepare('SELECT students.student_id, students.student_name, students.student_surname FROM students
+                            INNER JOIN topic_student ON students.student_id = topic_student.student_id
+                            INNER JOIN topics ON topic_student.topic_id = topics.topic_id 
+                            WHERE topics.topic_id = :topic_id
+                            ORDER BY students.student_surname ASC');
+                            $q->bindValue(':topic_id', $topic['topic_id']);
+                            $q->execute();
+                            $students = $q->fetchAll();
+                        
+                        } catch(Exception $ex){
+                                _response(500, 'System under maintainance', __LINE__);
+                                exit(); 
+                        }
+
+                        foreach($students as $student){?>
+                        <div>
+                            <a href="student-finished-activities.php?studentid=<?= $student['student_id'] ?>&topicid=<?= $topic['topic_id'] ?>"><?= $student['student_name'] ?> <?= $student['student_surname'] ?></a>
+                            <?php 
+                            
+                                try {
+
+                                    $q = $db->prepare('SELECT topic_student.student_id, count(*) FROM topic_student
+                                    WHERE topic_student.student_id = :student_id
+                                    GROUP BY topic_student.student_id;');
+                                    $q->bindValue(':student_id', $student['student_id']);
+                                    $q->execute();
+
+                                } catch(Exception $ex){
+                                    _response(500, 'System under maintainance', __LINE__);
+                                    exit(); 
+                                }
+                            
+                                while($topicCount = $q->fetch()){ ?>
+                                <p>(Number of active topics: <?= $topicCount['count(*)'] ?>)</p>
+                                <?php } ?>
+                        </div>
+                    <?php } ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 
 <script src="script.js"></script>
 
